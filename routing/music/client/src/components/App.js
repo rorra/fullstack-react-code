9794ae286292d10a728910f9c62f0f931abebcfc
Rodrigo Dominguez <rorra@rorra.com.ar>
2019-01-03 22:22:17 -0300

    Routing section done

--- routing/music/client/src/components/App.js
+++ routing/music/client/src/components/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 import TopBar from './TopBar';
+import PrivateRoute from './PrivateRoute';
 import AlbumsContainer from './AlbumsContainer';
+import Login from './Login';
+import Logout from './Logout';
 
 import '../styles/App.css';
 
 const App = () => (
   <div className='ui grid'>
-    <TopBar />
-    <div className='spacer row' />
+    <TopBar/>
+    <div className='spacer row'/>
     <div className='row'>
-      <AlbumsContainer />
+      <Switch>
+        <PrivateRoute path='/albums' component={ AlbumsContainer }/>
+        <Route path='/login' component={ Login }/>
+        <Route path='/logout' component={ Logout }/>
+
+        <Route exact path='/' render={ () => (
+          <Redirect
+            to='/albums'
+          />
+        ) }/>
+      </Switch>
     </div>
   </div>
 );
--- routing/music/client/src/components/Login.js
+++ routing/music/client/src/components/Login.js
@@ -6,10 +6,30 @@ import { Redirect } from 'react-router-dom';
 import { client } from '../Client';
 
 class Login extends Component {
+  state = {
+    loginInProgress: false,
+    shouldRedirect: false,
+  };
+
+  performLogin = () => {
+    this.setState({ loginInProgress: true });
+    client.login().then(() => (
+      this.setState({ shouldRedirect: true })
+    ));
+  };
+
+  redirectPath = () => {
+    const locationState = this.props.location.state;
+    const pathname = (
+      locationState && locationState.from && locationState.from.pathname
+    );
+    return pathname || '/albums';
+  };
+
   render() {
-    if ('todo') {
+    if (this.state.shouldRedirect) {
       return (
-        'todo'
+        <Redirect to={ this.redirectPath() }/>
       );
     } else {
       return (
@@ -17,13 +37,22 @@ class Login extends Component {
           <div className='ten wide column'>
             <div
               className='ui raised very padded text container segment'
-              style={{ textAlign: 'center' }}
+              style={ { textAlign: 'center' } }
             >
               <h2 className='ui green header'>
                 Fullstack Music
               </h2>
               {
-                /* todo */
+                this.state.loginInProgress ? (
+                  <div className='ui active centered inline loader'/>
+                ) : (
+                  <div
+                    className='ui large green submit button'
+                    onClick={ this.performLogin }
+                  >
+                    Login
+                  </div>
+                )
               }
             </div>
           </div>
